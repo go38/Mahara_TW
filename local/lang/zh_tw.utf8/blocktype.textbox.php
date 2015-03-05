d8c6b74752f8d10447596dc9f41e50252924b93d
Eric Hsin <eric.hsin@gmail.com>
2015-03-05 17:14:04 +0800

    U

--- local/lang/zh_tw.utf8/blocktype.textbox.php
+++ local/lang/zh_tw.utf8/blocktype.textbox.php
@@ -1,36 +1,27 @@
 <?php
 /**
- * Mahara: Electronic portfolio, weblog, resume builder and social networking
- * Copyright (C) 2006-2008 Catalyst IT Ltd (http://www.catalyst.net.nz)
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package    mahara
- * @subpackage lang/zh_tw.utf8
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL
- * @author     
- * @copyright  
+ * @subpackage blocktype-textbox
+ * @author     Catalyst IT Ltd
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL version 3 or later
+ * @copyright  For copyright information on Mahara, please see the README file distributed with this software.
  *
  */
 
 defined('INTERNAL') || die();
 
-$string['blockcontent'] = '區塊內容';
+$string['title'] = '筆記';
 $string['description'] = '增加筆記到您的頁面';
-$string['makeacopy'] = '製作副本';
-$string['managealltextboxcontent'] = '管理全部文字盒內容';
-$string['readonlymessage'] = '您選擇的文字在這頁面不能編輯。';
+$string['blockcontent'] = '區塊內容';
+$string['usecontentfromanothertextbox1'] = '使用其他筆記的內容';
 $string['textusedinotherblocks'] = '如果您編輯這區塊的文字內容，也將會一起變更在其他 %s 的區塊內容。';
-$string['title'] = '文字盒';
-$string['usecontentfromanothertextbox'] = '使用其他文字盒的內容';
+$string['managealltextboxcontent1'] = '管理所有筆記內容';
+$string['readonlymessage'] = '您選擇的文字在這頁面不能編輯。';
+$string['makeacopy'] = '製作副本';
+
+$string['textusedinothernotes'] = array(
+    0 => 'If you edit the text of this note, it will also be changed in %s block where it appears.',
+    1 => 'If you edit the text of this note, it will also be changed in %s blocks where it appears.'
+);
+
